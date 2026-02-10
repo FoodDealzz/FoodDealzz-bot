7402a77487ed24ea851864cc4636d3987f524e17
FoodDealzz <rannaba291@gmail.com>
2026-02-10 14:29:24 +0100

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import os
-import threading
 from flask import Flask
-
-from bot import run_bot  # on importe la fonction qui lance le bot
+from bot import run_bot
 
 app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "OK"
+    return "OK", 200
+
+# Démarre le bot 24/24 en arrière-plan
+run_bot()
 
 if __name__ == "__main__":
-    # lance le bot dans un thread (ok car run_bot va gérer son propre asyncio)
-    threading.Thread(target=run_bot, daemon=True).start()
-
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", "10000"))
     app.run(host="0.0.0.0", port=port)
