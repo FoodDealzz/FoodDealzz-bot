9a7d9a5a0b6f6ea1bea0bed782d91121b2a009dd
FoodDealzz <rannaba291@gmail.com>
2026-02-11 03:25:07 +0100

    app.py

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 import os
-import threading
 from flask import Flask
-
-from bot import main  # on démarre le bot depuis bot.py
+from threading import Thread
+from bot import run_bot
 
 app = Flask(__name__)
+
+# Lance le bot une seule fois (thread)
+Thread(target=run_bot, daemon=True).start()
 
 @app.get("/")
 def home():
     return "OK"
 
-def run_bot():
-    main()
-
 if __name__ == "__main__":
-    # Lance le bot en arrière-plan
-    threading.Thread(target=run_bot, daemon=True).start()
-
-    # Obligatoire pour Render Web Service
-    port = int(os.environ.get("PORT", "10000"))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
